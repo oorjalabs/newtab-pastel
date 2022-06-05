e589a2005ae992e9a07900caede80f8bd1651870
Aditya <ab.bulk@gmail.com>
2022-06-05 18:04:50 +0100

    Remove js files and add to gitignore

--- src/js/onloadTheme.js
+++
@@ -1,9 +0,0 @@
-// To prevent bright flashing when opening new tab with dark mode enabled
-const useDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-if (useDark) {
-    document.body.classList.add("system_init_dark");
-}
-else {
-    document.body.classList.remove("system_init_dark");
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoib25sb2FkVGhlbWUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJvbmxvYWRUaGVtZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSx5RUFBeUU7QUFDekUsTUFBTSxPQUFPLEdBQUcsTUFBTSxDQUFDLFVBQVUsSUFBSSxNQUFNLENBQUMsVUFBVSxDQUFDLDhCQUE4QixDQUFDLENBQUMsT0FBTyxDQUFDO0FBQy9GLElBQUksT0FBTyxFQUFFO0lBQ1QsUUFBUSxDQUFDLElBQUksQ0FBQyxTQUFTLENBQUMsR0FBRyxDQUFDLGtCQUFrQixDQUFDLENBQUM7Q0FDbkQ7S0FBTTtJQUNILFFBQVEsQ0FBQyxJQUFJLENBQUMsU0FBUyxDQUFDLE1BQU0sQ0FBQyxrQkFBa0IsQ0FBQyxDQUFDO0NBQ3REIn0=
